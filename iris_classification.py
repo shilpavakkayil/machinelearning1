bb26e9845ccd01046514d16bd6edc6ccc90d1d12
shilpavakkayil <134934984+shilpavakkayil@users.noreply.github.com>
2023-08-08 00:29:30 +0100

    Add files via upload

---
+++ iris_classification.py
@@ -0,0 +1,4 @@
+import pandas as pd
+if __name__=='__main__':
+    url = "https://raw.githubusercontent.com/jbrownlee/Datasets/master/iris.csv"
+    names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
