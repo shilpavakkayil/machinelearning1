e856dc619c2d466b1f938a16e981dbe3ed2ccf65
shilpavakkayil <shilpavpurushothaman@gmail.com>
2023-08-10 00:06:22 +0100

    commit

--- iris_classification.py
+++ iris_classification.py
@@ -1,4 +1,8 @@
 import pandas as pd
 if __name__=='__main__':
-    url = "https://raw.githubusercontent.com/jbrownlee/Datasets/master/iris.csv"
+    #url = "https://raw.githubusercontent.com/jbrownlee/Datasets/master/iris.csv"
     names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
+    dataset = pd.read_csv("iris_dataset.csv", names=names)
+    print(dataset.shape)
+    print(dataset.head())
+    print(dataset.describe())
