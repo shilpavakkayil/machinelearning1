92cd73880c34c589b8673613a24dcf68e42bd060
shilpavakkayil <shilpavpurushothaman@gmail.com>
2023-10-01 19:14:18 +0100

    commit

--- iris_classification.py
+++ iris_classification.py
@@ -1,8 +1,38 @@
-import pandas as pd
-if __name__=='__main__':
-    #url = "https://raw.githubusercontent.com/jbrownlee/Datasets/master/iris.csv"
-    names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
-    dataset = pd.read_csv("iris_dataset.csv", names=names)
-    print(dataset.shape)
-    print(dataset.head())
-    print(dataset.describe())
+from numpy import argmax
+from pandas import read_csv
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+from tensorflow.keras import Sequential
+from tensorflow.keras.layers import Dense
+import numpy as np
+# load the dataset
+
+df = read_csv("iris_dataset.csv", header=None)
+# split into input and output columns
+X, y = df.values[1:, :-1], df.values[1:, -1]
+# ensure all data are floating point values
+X = X.astype('float32')
+# encode strings to integer
+y = LabelEncoder().fit_transform(y)
+# split into train and test datasets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33)
+print(X_train.shape, X_test.shape, y_train.shape, y_test.shape)
+# determine the number of input features
+n_features = X_train.shape[1]
+# define model
+model = Sequential()
+model.add(Dense(10, activation='relu', kernel_initializer='he_normal',input_shape=(n_features,)))
+model.add(Dense(8, activation='relu', kernel_initializer='he_normal'))
+model.add(Dense(3, activation='softmax'))
+# compile the model
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+# fit the model
+model.fit(X_train, y_train, epochs=150, batch_size=32, verbose=0)
+# evaluate the model
+loss, acc = model.evaluate(X_test, y_test, verbose=0)
+print('Test Accuracy: %.3f' % acc)
+# make a prediction
+row1 = [5.1,3.5,1.4,0.2]
+row2 = np.array([[5.1,3.5,1.4,0.2]])
+yhat = model.predict([row2])
+print('Predicted: %s (class=%d)' % (yhat, argmax(yhat)))
